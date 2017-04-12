81a9f52d103bd40180182af14f8b7274611efb18
alaap96 <alaap96@gmail.com>
2017-04-12 16:32:16 -0500

    HackerRank

---
+++ DS/Linked List/Delete duplicate-value nodes from a sorted linked list.java
@@ -0,0 +1,25 @@
+/*
+Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+
+Node RemoveDuplicates(Node head) {
+  // This is a "method-only" submission. 
+  // You only need to complete this method. 
+    Node current = head;
+    if(current == null){
+        return head;
+    }
+    while(current != null && current.next != null ){
+        if(current.data == current.next.data){
+            current.next = current.next.next;    
+        }    
+        else{
+            current = current.next;
+        }
+    } 
+    return head;
+}
